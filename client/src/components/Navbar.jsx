ec24e742080533ed162745fa19f1df2aeae5cfed
Gokul Elumalai <gokulelumalai46@gmail.com>
2025-07-04 20:26:24 +0530

    added the logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
         {/* Logo */}
         <div className="flex items-center gap-2">
           <img
-            src="https://i.ibb.co/ccJNk82/logo.png"
+            src="/logo.jpg"
             alt="SafeHands Logo"
             className="h-8 w-8 rounded-full"
           />
